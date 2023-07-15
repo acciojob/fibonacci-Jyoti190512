d36777f872a85978257eb8e9266cdd0acba4a2b1
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-15 21:39:41 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function fibonacci(num) {
 // your code here
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let prev1 = 0;
+  let prev2 = 1;
+  let result;
+
+  for (let i = 2; i <= num; i++) {
+    result = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = result;
+  }
+
+  return result;
+}
+
+// Test the function
+const n = 10; // Change n to any positive integer
+const result = fibonacci(n);
+console.log(`The ${n}th Fibonacci number is: ${result}`);
+
 }
 
 module.exports = fibonacci;
