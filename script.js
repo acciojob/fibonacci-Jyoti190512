59cae47345e8ffa9d32ead624cc0c8b82b9a835f
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-18 09:48:48 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+function fibonacci(num) {
+  if (num <= 1) {
+    return num;
+  }
+  
+  let prev = 0;
+  let curr = 1;
+  
+  for (let i = 2; i <= num; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  
+  return curr;
+}
 
 
